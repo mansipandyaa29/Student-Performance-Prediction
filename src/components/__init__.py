5940486bdf732b01a835bd740c49bed7b6b93368
Mansi Pandya <mansipandya@Mansis-Air.lan>
2024-01-07 23:51:11 -0800

    Model Training

--- src/components/__init__.py
+++ src/components/__init__.py
@@ -1,8 +1,14 @@
 from src.components.data_ingestion import DataIngestion
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 
 obj = DataIngestion()
 train_data,test_data = obj.initiate_data_ingestion()
 
 data_transformation = DataTransformation()
-data_transformation.initiate_data_transformation(train_data,test_data)
+train_array,test_array,preprocessor_path = data_transformation.initiate_data_transformation(train_data,test_data)
+
+model_training = ModelTrainer()
+print(model_training.initiate_model_trainer(train_array,test_array))
+
+
