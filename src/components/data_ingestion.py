86a26a0a59411e3e352dce55b1cc0b521b5a0e59
mansipandyaa29 <mansipandyaa29@gmail.com>
2024-04-23 18:31:25 -0700

    data ingestion updated

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,8 +6,6 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-from src.components.data_transformation import DataTransformation, DataTransformationConfig
-
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts',"train.csv")
